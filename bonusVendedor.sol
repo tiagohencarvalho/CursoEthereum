f0c1d93051e4a3709f95f9f1a349a61907773e26
tiagohencarvalho <136171246+tiagohencarvalho@users.noreply.github.com>
2023-06-10 10:03:33 -0300

    Update bonusVendedor.sol

--- bonusVendedor.sol
+++ bonusVendedor.sol
@@ -6,16 +6,21 @@ contract bonusVendedor {
     string public nomeVendedor;
     uint256  public valorDeBonus;
     uint256  private valorVenda;
-    uint256 constant fatorBonus = 10;
+    uint256 public fatorBonus;
 
+        constructor(string memory _nomeVendedor,uint256 _fatorBonus, uint256 _valorVenda)  {
+        nomeVendedor = _nomeVendedor;
+        valorVenda = _fatorBonus;
+        valorVenda = _valorVenda;
+    }
 
     function valorDeBonificacao() public returns(uint256) {
 
-        valorDeBonus = valorVenda*(fatorBonus/100);
+        valorDeBonus = valorVenda*(fatorBonus/100) + valorVenda;
 
         return valorDeBonus;
     } 
 
 }
 
-// 0x66FF7Bd333CF2431A6Bc8Db65816D69d724Ba4CD
+// 0x7a14081375471f60b60D5c68adcc542BA436ED6d
